6f0d6ed786e171cf06d2bcd270ce11a090ed5a94
Tim-Harrington <tjh5415@gmail.com>
2022-06-06 03:43:09 +0000

    Modify SQL
    Changing the values for the sql model

--- create_model.sql
+++ create_model.sql
@@ -1,23 +1,23 @@
 CREATE OR REPLACE MODEL `my_models.annual_temp`
 OPTIONS
   (model_type='ARIMA_PLUS',
-  time_series_timestamp_col = 'YEAR',
-  time_series_data_col = 'avg_temp_CS',
+  time_series_timestamp_col = 'year_dt',
+  time_series_data_col = 'avg_temp',
   auto_arima = TRUE,
   data_frequency = "AUTO_FREQUENCY",
   decompose_time_series = TRUE
   ) AS
 
 select
-avg(arithmetic_mean) as avg_temp_CS
-,YEAR
+avg(arithmetic_mean) as avg_temp
+,CAST(CONCAT(CAST(year as STRING), '-01-01') AS DATE) AS year_dt
 FROM `bigquery-public-data.epa_historical_air_quality.air_quality_annual_summary`
 where
-parameter_name = 'Average Ambient Temperature'
-and metric_used = 'observed_values'
+parameter_name = 'Outdoor Temperature'
+and metric_used = 'Observed Values'
 and completeness_indicator = "Y"
 and year between 1990 and 2020
 group by
-YEAR
+year_dt
 order by
-YEAR
+year_dt
